b2417984fb45a297f54649124a89040c299ad495
Manfred Von Richthofen <pksubscriber@gmail.com>
2023-05-23 14:16:27 -0400

    removed next.js - moved to KOA

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+let koa = require('koa'),
+    koarouter = require('koa-router'),
+    path = require('path'),
+    render = require('koa-ejs'),
+    serve = require("koa-static"),
+    mount = require("koa-mount");
+
+
+const PORT = 3000;
+const app = new koa();
+const router = new koarouter();
+
+app.use(serve(__dirname + '/public'));
+
+// Routes
+app.use(router.routes()).use(router.allowedMethods());
+render(app, {
+    root: path.join(__dirname, 'views'),
+    public: path.join(__dirname, 'public'),
+    layout: 'layout',
+    viewExt: 'html',
+    cache: false,
+    debug: false 
+});
+
+
+
+
+//--------------------------------------------------
+//TODO Move this to a diff class
+
+
+router.get('/', async ctx => {
+    await ctx.render('index', {showLayout: false});
+});
+
+router.get('/story', async ctx => {
+    await ctx.render('story', {showLayout: true});
+});
+//--------------------------------------------------
+
+//app.use(async ctx => ctx.body = "HELLO");
+
+app.listen(PORT, () => console.log('Application Activated, Listening on PORT : ' + PORT));
